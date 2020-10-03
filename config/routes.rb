ae6baf975229c282cf2327f60c3d3393d2f3a622
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-04 00:35:16 +0100

    add profile page and design

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "posts#index"
+  resources :users, only: [:show], as: "profile"
+  get "about" => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
