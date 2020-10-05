903691d78aab8d5b3ee53290d38e69ce329b08c9
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-05 15:09:18 +0100

    add embeded comments to post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,23 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
+  
   validates_presence_of :body
 
   def comments
     Comment.where(commentable: commentable, parent_id: id)
   end
+
+# Deleting/ Replaceing Comments In Nested Threads Not Working
+# 
+# Replace belongs_to :user with belongs_to :user, optional: true
+#
+# def destroy
+#   update(user: nil, body: nil)
+# end
+#
+#  def deleted?
+#    user.nil?
+#  end
+
 end
