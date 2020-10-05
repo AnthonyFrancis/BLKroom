71668949ee3780561f7ef0a244974ea0712968e8
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-05 21:04:47 +0100

    test carrier fog method

--- app/uploaders/post_video_uploader.rb
+++ app/uploaders/post_video_uploader.rb
@@ -4,8 +4,14 @@ class PostVideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  
+  def development
+    storage :file
+  end 
+
+  def production
+    storage :fog
+  end 
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
