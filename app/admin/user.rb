af83dc3a7a4eeee2ebb8067a2390c8aa0555d77a
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-14 21:01:34 +0100

    fixed active admin edit user without password

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -60,6 +60,14 @@ controller do
   super
   end
 
+  def update
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+    super
+  end
+
 end
 
 end
