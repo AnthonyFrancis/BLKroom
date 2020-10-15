003062e67ec168acc3262bb3cd253da44c9a8dca
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-16 00:27:00 +0100

    sendgrid test

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'BLKroom',
+  :user_name => 'apikey',
   :password => 'your_sendgrid_api_key',
   :domain => 'anthonyfrancis.co',
   :address => 'smtp.sendgrid.net',
