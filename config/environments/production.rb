6048ed9d9e4c31598b37a610094cf682d110b9dc
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-16 14:31:08 +0100

    sendgrid test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,8 +114,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :user_name => 'SENDGRID_USERNAME',
-    :password => 'SENDGRID_PASSWORD',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'blkroom-dev.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
