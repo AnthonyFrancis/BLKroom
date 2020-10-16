618a903cc3993bd1163ecfd6be0ecf69bc00bca1
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-16 10:22:14 +0100

    sendgrid test

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
   :password => 'your_sendgrid_api_key',
-  :domain => 'anthonyfrancis.co',
+  :domain => 'blkroom-dev.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
