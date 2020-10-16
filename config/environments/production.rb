71e0944d75ea354f0fd56de8c12420b634178789
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-16 19:47:52 +0100

    add blkroom email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,13 +110,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'blkroom-dev.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'blkroom.net' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'blkroom-dev.herokuapp.com',
+    :domain => 'blkroom.net',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
