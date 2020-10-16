616d0c246fcf8831cb172bb3d13d71544741ccf0
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-16 11:42:51 +0100

    send test

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => 'your_sendgrid_api_key',
-  :domain => 'blkroom-dev.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => 'your_sendgrid_api_key',
+    :domain => 'blkroom-dev.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
