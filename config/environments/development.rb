1577f279aef7333fa17f833bdc10c6612a14e5fc
“Anthony”Francis” <tonytrack@gmail.com>
2020-10-18 00:55:43 +0100

    everioment test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,17 +60,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :user_name => 'SENDGRID_USERNAME',
-    :password => 'your_sendgrid_api_key',
-    :domain => 'blkroom-dev.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
