661bf9d477e176777d8039a3a033369305f2cffb
“Anthony”Francis” <tonytrack@gmail.com>
2020-12-10 12:08:55 +0000

    fixed invite on post page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
   def author_of(record)
     user_signed_in? && current_user.id == record.user_id
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
